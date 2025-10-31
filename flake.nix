ff926908053ee1401efc930e0238210b7ddba65e
Pol Dellaiera <pol.dellaiera@protonmail.com>
2025-10-31 16:19:14 +0100

    refactor: use `flake.parts` framework

--- flake.nix
+++ flake.nix
@@ -5,25 +5,26 @@
     nixpkgs.url = "github:NixOS/nixpkgs";
     # Provides a list of systems supported by Nix (x86_64-linux, aarch64-linux, etc.).
     systems.url = "github:nix-systems/default";
+    # flake-parts helps structure the flake outputs.
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   # The outputs function defines what this flake provides.
   # Its sole parameter is `inputs`, which contains the resolved inputs.
   outputs =
     inputs:
-    let
-      # Helper to generate attributes for multiple system architectures.
-      eachSystem = inputs.nixpkgs.lib.genAttrs (import inputs.systems);
-    in
-    {
-      # Define development shells for each architecture.
-      devShells = eachSystem (system: {
-        default =
-          let
-            # Get the package set for the current system architecture.
-            pkgs = inputs.nixpkgs.legacyPackages.${system};
-          in
-          pkgs.mkShell {
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      # The `systems` list determines which platforms to support.
+      systems = import inputs.systems;
+
+      # Define per-system modules.
+      # It is a function that takes `pkgs` and many other parameters.
+      # The `pkgs` parameter is the package set for the given system architecture.
+      perSystem =
+        { pkgs, ... }:
+        {
+          # Development shell definition.
+          devShells.default = pkgs.mkShell {
             # List the packages to be available in the shell.
             packages = [
               pkgs.php
@@ -37,6 +38,6 @@
               echo ""
             '';
           };
-      });
+        };
     };
 }
