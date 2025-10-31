5c18075c8bece8706a55e1a248a482eced2dd71d
Pol Dellaiera <pol.dellaiera@protonmail.com>
2025-10-31 16:28:41 +0100

    refactor: use `vic/import-tree`, each file is a module now

--- flake.nix
+++ flake.nix
@@ -7,16 +7,11 @@
     systems.url = "github:nix-systems/default";
     # flake-parts helps structure the flake outputs.
     flake-parts.url = "github:hercules-ci/flake-parts";
+    # import-tree helps to import all Nix files from a directory.
+    import-tree.url = "github:vic/import-tree";
   };
 
   # The outputs function defines what this flake provides.
   # Its sole parameter is `inputs`, which contains the resolved inputs.
-  outputs =
-    inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [
-        ./modules/devshells/php/devshell.php.nix
-        ./modules/systems.nix
-      ];
-    };
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
 }
