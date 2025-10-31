bf2c195e96a08a40e91631b84645523aea14c444
Pol Dellaiera <pol.dellaiera@protonmail.com>
2025-10-31 15:55:09 +0100

    refactor: use flake
    Run `nix develop` or `nix-shell` to enter the default development shell

--- shell.nix
+++ shell.nix
@@ -1,46 +1,12 @@
-# This file is a function that takes an argument `pkgs`, which is a set of
-# Nix packages. If `pkgs` is not provided, it defaults to importing an
-# instance of the `NixOS/nixpkgs` repository at a specific moment in time.
-#
-# It returns the result of calling `pkgs.mkShell`, which creates a development
-# shell environment with the specified packages and a shell hook.
-{
-  pkgs ? (
-    import (fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/bb3f17b14b21e1f08bd7b5d8e1c37b84066c235f.tar.gz";
-      sha256 = "sha256:0y7b9kdqrnq11401v4yf8m3ndk8ijcpxbyikmgh7n22nn1v0hyd1";
-    }) { }
-  ),
-}:
-
-{
-  php = pkgs.mkShell {
-    # List the packages to be available in the shell.
-    packages = [
-      pkgs.php
-      pkgs.php.packages.composer
-    ];
-
-    # Optional: Run commands when the shell starts.
-    shellHook = ''
-      echo ""
-      echo ">> PHP development environment is ready!"
-      echo ""
-    '';
-  };
-
-  go = pkgs.mkShell {
-    # List the packages to be available in the shell.
-    packages = [
-      pkgs.go
-      pkgs.gotools
-    ];
-
-    # Optional: Run commands when the shell starts.
-    shellHook = ''
-      echo ""
-      echo ">> Go development environment is ready!"
-      echo ""
-    '';
-  };
-}
+(import (
+  let
+    lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+    nodeName = lock.nodes.root.inputs.flake-compat;
+  in
+  fetchTarball {
+    url =
+      lock.nodes.${nodeName}.locked.url
+        or "https://github.com/NixOS/flake-compat/archive/${lock.nodes.${nodeName}.locked.rev}.tar.gz";
+    sha256 = lock.nodes.${nodeName}.locked.narHash;
+  }
+) { src = ./.; }).outputs.devShells.x86_64-linux
