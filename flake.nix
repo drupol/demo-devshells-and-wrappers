aab204ebf893ce8b245da3c23f08c5bd8fe1c56d
Pol Dellaiera <pol.dellaiera@protonmail.com>
2025-10-31 16:28:41 +0100

    refactor: use `vic/import-tree`, each file is a module now

--- flake.nix
+++ flake.nix
@@ -7,37 +7,11 @@
     systems.url = "github:nix-systems/default";
     # flake-parts helps structure the flake outputs.
     flake-parts.url = "github:hercules-ci/flake-parts";
+    # import-tree helps to import all Nix files from a directory.
+    import-tree.url = "github:vic/import-tree";
   };
 
   # The outputs function defines what this flake provides.
   # Its sole parameter is `inputs`, which contains the resolved inputs.
-  outputs =
-    inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      # The `systems` list determines which platforms to support.
-      systems = import inputs.systems;
-
-      # Define per-system modules.
-      # It is a function that takes `pkgs` and many other parameters.
-      # The `pkgs` parameter is the package set for the given system architecture.
-      perSystem =
-        { pkgs, ... }:
-        {
-          # Development shell definition.
-          devShells.default = pkgs.mkShell {
-            # List the packages to be available in the shell.
-            packages = [
-              pkgs.php
-              pkgs.php.packages.composer
-            ];
-
-            # Optional: Run commands when the shell starts.
-            shellHook = ''
-              echo ""
-              echo ">> PHP development environment is ready!"
-              echo ""
-            '';
-          };
-        };
-    };
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
 }
