a536b42a89465a4f457297cbd587ef8e16cd84f6
Pol Dellaiera <pol.dellaiera@protonmail.com>
2025-10-31 16:19:14 +0100

    refactor: use `flake.parts` framework

--- flake.nix
+++ flake.nix
@@ -8,52 +8,55 @@
       url = "github:NixOS/flake-compat";
       flake = false;
     };
+    # flake-parts helps structure the flake outputs.
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   # The outputs function defines what this flake provides.
   # Its sole parameter is `inputs`, which contains the resolved inputs.
   outputs =
     inputs:
-    let
-      # Alias for the Nixpkgs library.
-      lib = inputs.nixpkgs.lib;
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      # The `systems` list determines which platforms to support.
+      systems = inputs.nixpkgs.lib.systems.flakeExposed;
 
-      # Helper to generate attributes for multiple system architectures.
-      # Documentation: https://noogle.dev/f/lib/genAttrs
-      forAllSystems =
-        fn: lib.genAttrs lib.systems.flakeExposed (system: fn inputs.nixpkgs.legacyPackages.${system});
-    in
-    {
-      # Define development shells for each architecture.
-      devShells = forAllSystems (pkgs: {
-        php = pkgs.mkShell {
-          # List the packages to be available in the shell.
-          packages = [
-            pkgs.php
-            pkgs.php.packages.composer
-          ];
+      # Define per-system modules.
+      # It is a function that takes `pkgs` and many other parameters.
+      # The `pkgs` parameter is the package set for the given system architecture.
+      perSystem =
+        { pkgs, ... }:
+        {
+          # Development shell definitions.
+          devShells = {
+            php = pkgs.mkShell {
+              # List the packages to be available in the shell.
+              packages = [
+                pkgs.php
+                pkgs.php.packages.composer
+              ];
 
-          # Optional: Run commands when the shell starts.
-          shellHook = ''
-            echo ""
-            echo ">> PHP development environment is ready!"
-            echo ""
-          '';
+              # Optional: Run commands when the shell starts.
+              shellHook = ''
+                echo ""
+                echo ">> PHP development environment is ready!"
+                echo ""
+              '';
+            };
+            go = pkgs.mkShell {
+              # List the packages to be available in the shell.
+              packages = [
+                pkgs.go
+                pkgs.gotools
+              ];
+
+              # Optional: Run commands when the shell starts.
+              shellHook = ''
+                echo ""
+                echo ">> Go development environment is ready!"
+                echo ""
+              '';
+            };
+          };
         };
-        go = pkgs.mkShell {
-          # List the packages to be available in the shell.
-          packages = [
-            pkgs.go
-            pkgs.gotools
-          ];
-
-          # Optional: Run commands when the shell starts.
-          shellHook = ''
-            echo ""
-            echo ">> Go development environment is ready!"
-            echo ""
-          '';
-        };
-      });
     };
 }
