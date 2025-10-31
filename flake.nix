bcd01fdd6a1903a8eb531cc18e2a33c161db0f6b
Pol Dellaiera <pol.dellaiera@protonmail.com>
2025-10-31 16:13:04 +0100

    refactor: update flake, the devShell is available for more architecture now

--- flake.nix
+++ flake.nix
@@ -12,27 +12,39 @@
 
   # The outputs function defines what this flake provides.
   # Its sole parameter is `inputs`, which contains the resolved inputs.
-  outputs = inputs: {
-    # Define a development shell for x86_64-linux architecture only.
-    # This means that this shell will only be available on 64-bit Linux systems.
-    devShells.x86_64-linux.default =
-      let
-        # Get the package set for the current system architecture.
-        pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
-      in
-      pkgs.mkShell {
-        # List the packages to be available in the shell.
-        packages = [
-          pkgs.php
-          pkgs.php.packages.composer
-        ];
+  outputs =
+    inputs:
+    let
+      # Helper to generate attributes for multiple system architectures.
+      eachSystem = inputs.nixpkgs.lib.genAttrs [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+    in
+    {
+      # Define development shells for each architecture.
+      devShells = eachSystem (system: {
+        default =
+          let
+            # Get the package set for the current system architecture.
+            pkgs = inputs.nixpkgs.legacyPackages.${system};
+          in
+          pkgs.mkShell {
+            # List the packages to be available in the shell.
+            packages = [
+              pkgs.php
+              pkgs.php.packages.composer
+            ];
 
-        # Optional: Run commands when the shell starts.
-        shellHook = ''
-          echo ""
-          echo ">> PHP development environment is ready!"
-          echo ""
-        '';
-      };
-  };
+            # Optional: Run commands when the shell starts.
+            shellHook = ''
+              echo ""
+              echo ">> PHP development environment is ready!"
+              echo ""
+            '';
+          };
+      });
+    };
 }
