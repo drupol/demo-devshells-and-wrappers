c4a103d4c1135c7378f7f581843bb470afc94116
Pol Dellaiera <pol.dellaiera@protonmail.com>
2025-11-01 18:41:07 +0100

    feat: add `my-custom-config` NixOS configuration

--- modules/pkgs.nix
+++ modules/pkgs.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ inputs, withSystem, ... }:
 {
   imports = [ inputs.pkgs-by-name-for-flake-parts.flakeModule ];
 
@@ -6,5 +6,18 @@
     # This is a specific option defined by the pkgs-by-name-for-flake-parts module only.
     # It tells the module where to find the package directory in your project.
     pkgsDirectory = ../pkgs;
+  };
+
+  flake = {
+    # This expose a "default" overlay that can be used in Nix configurations
+    # to get access to the packages defined in the ../pkgs/ directory.
+    overlays.default =
+      _final: prev:
+      withSystem prev.stdenv.hostPlatform.system (
+        { config, ... }:
+        {
+          local = config.packages;
+        }
+      );
   };
 }
