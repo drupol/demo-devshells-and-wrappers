bd5bf93c9f66ac7a9799f131a2b08d98e70ef0a5
Pol Dellaiera <pol.dellaiera@protonmail.com>
2025-11-03 08:05:26 +0100

    refactor: split files in different modules

--- flake.nix
+++ flake.nix
@@ -14,30 +14,9 @@
   outputs =
     inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      # The `systems` list determines which platforms to support.
-      systems = import inputs.systems;
-
-      # Define per-system modules.
-      # It is a function that takes `pkgs` and many other parameters.
-      # The `pkgs` parameter is the package set for the given system architecture.
-      perSystem =
-        { pkgs, ... }:
-        {
-          # Development shell definition.
-          devShells.php = pkgs.mkShell {
-            # List the packages to be available in the shell.
-            packages = [
-              pkgs.php
-              pkgs.php.packages.composer
-            ];
-
-            # Optional: Run commands when the shell starts.
-            shellHook = ''
-              echo ""
-              echo ">> PHP development environment is ready!"
-              echo ""
-            '';
-          };
-        };
+      imports = [
+        ./modules/devshells/php/devshell.php.nix
+        ./modules/systems.nix
+      ];
     };
 }
---
+++ modules/systems.nix
@@ -0,0 +1,5 @@
+{ inputs, ... }:
+{
+  # The `systems` list determines which platforms to support.
+  systems = import inputs.systems;
+}
